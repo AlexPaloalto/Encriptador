a876183c6f707160972f0b3c2fbdd040dcddb00d
AlexPaloalto <alex0297_pl@ciencias.unam.mx>
2023-05-31 13:46:45 -0600

    Ahora se logra cambiar cada una de las vocales

--- funciones.js
+++ funciones.js
@@ -1,7 +1,16 @@
+var vocales = {
+    a:"ai",
+    e:"enter",
+    i:"imes",
+    o:"ober",
+    u:"ufat"
+}
 
 function guardarTexto(){
     var texto = document.getElementById("texto-inicio").value;
-    var texto_modificado = texto.replace(/a/g, "ai");
+    var texto_modificado = texto.replace(/a|e|i|o|u/g, function(matched){
+        return vocales[matched];
+    });
     document.getElementById("resultado").value =  texto_modificado;
 }
 
