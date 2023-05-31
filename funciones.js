70d2807755766eab2650f4fe8d8cac761a9df188
AlexPaloalto <alex0297_pl@ciencias.unam.mx>
2023-05-31 15:34:45 -0600

    Se desencripta texto y se puede copiar el resultado

--- funciones.js
+++ funciones.js
@@ -6,6 +6,14 @@ var vocales = {
     u:"ufat"
 }
 
+var vocales_inv = {
+    ai:"a",
+    enter:"e",
+    imes:"i",
+    ober:"o",
+    ufat:"u"
+}
+
 function guardarTexto(){
     var texto = document.getElementById("texto-inicio").value;
     var texto_modificado = texto.replace(/a|e|i|o|u/g, function(matched){
@@ -14,3 +22,19 @@ function guardarTexto(){
     document.getElementById("resultado").value =  texto_modificado;
 }
 
+function desencriptarTexto(){
+    var texto_encrip = document.getElementById("texto-inicio").value;
+    var texto_excrip_modificado = texto_encrip.replace(/ai|enter|imes|ober|ufat/g, function(matched){
+        return vocales_inv[matched];
+    });
+    document.getElementById("resultado").value = texto_excrip_modificado;
+}
+
+
+function copiarTexto(){
+    var texto2 = document.getElementById("resultado");
+    texto2.select();
+    texto2.setSelectionRange(0,99999);
+    document.execCommand("copy");
+
+}
